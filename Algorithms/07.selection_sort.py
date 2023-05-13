0da49a99de2f55f903e327d5460f903c4caa9284
Adrian Torj-Kobza <42949599+AdrianTorjKobza@users.noreply.github.com>
2023-05-13 12:44:51 +0300

    Add files via upload

---
+++ Algorithms/07.selection_sort.py
@@ -0,0 +1,30 @@
+
+# Selection Sort algo.
+# Sorting algorithm that works by repeatedly finding the minimum element,
+# from an unsorted portion of an array and moving it to the beginning of the sorted portion.
+def selection_sort(list):
+    sorted_list = []
+    list_length = len(list)
+
+    for i in range(0, list_length):
+        index_to_move = index_of_min(list)
+        min_index = list.pop(index_to_move)
+        sorted_list.append(min_index)
+    
+    return sorted_list
+
+
+def index_of_min(list):
+    min_index = 0
+    list_length = len(list)
+
+    for i in range (1, list_length):
+        if list[i] < list[min_index]:
+            min_index = i
+    
+    return min_index
+
+numbers = [9, 2, 5, 1, 0, 7]
+result = selection_sort(numbers)
+print(result)
+# [0, 1, 2, 5, 7, 9]
