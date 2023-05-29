5699726a407bb4190ba95fa71a4269f7d73dcb13
Adrian Torj-Kobza <42949599+AdrianTorjKobza@users.noreply.github.com>
2023-05-29 20:38:53 +0300

    Add files via upload

---
+++ LeetCode/0058. Length of Last Word/main.py
@@ -0,0 +1,19 @@
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        count = 0
+        
+        for i in range(len(s) -1, -1, -1):
+            if s[i] == ' ':
+                if count > 0:
+                    return count
+            else:
+                count = count + 1
+
+        return count
+
+string_input = "   fly me   to   the moon  "
+
+solution = Solution()
+word_length = solution.lengthOfLastWord(string_input)
+print("Length of the last word:", word_length) # Length of the last word: 4
